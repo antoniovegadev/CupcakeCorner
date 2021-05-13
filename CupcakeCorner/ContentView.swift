957971263c13f8cebb4a58f363a527bdd95cc0ad
antonivegaochoa <tonivegaochoa@gmail.com>
2021-05-13 17:22:33 -0400

    validating and disabling forms

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -36,44 +36,27 @@ struct Result: Codable {
 }
 
 struct ContentView: View {
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
 
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .onAppear(perform: loadData)
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
     
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=bad+bunny&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data – go back to the main thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        self.results = decodedResponse.results
-                    }
-
-                    // everything is good, so we can exit
-                    return
-                }
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
 
-            // if we're still here it means there was a problem
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+            }
+            .disabled(disableForm)
+        }
     }
 }
 
