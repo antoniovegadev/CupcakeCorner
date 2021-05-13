385ef07f6eb24a8886708a1d2a7e31301d3412ca
antonivegaochoa <tonivegaochoa@gmail.com>
2021-05-13 19:54:39 -0400

    completed day 50

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,39 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Antonio Vega on 5/13/21.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+                    
+                    Text("Your total is $\(order.cost, specifier: "%.2f")")
+                        .font(.title)
+
+                    Button("Place Order") {
+                        // place the order
+                    }
+                    .padding()
+                }
+            }
+        }
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
